85221cefdc506bb33d26015cf1ab7f903d373616
IanVermeulen <ian.vermeulen93@gmail.com>
2016-09-06 20:44:06 +0200

    fixed calendar

--- www/js/controllers/calendar.js
+++ www/js/controllers/calendar.js
@@ -1,5 +1,32 @@
 angular.module('roots.controllers')
 
-.controller('CalendarCtrl', function($scope, $timeout, $rootScope, $sce, $localstorage, $ionicModal, $ionicPopup, $ionicLoading, $location) {
+.controller('CalendarCtrl', function($scope, $timeout, $rootScope, $sce, $localstorage, $ionicModal, $ionicPopup, $ionicLoading, $location, Calendar) {
+    
+    $scope.doRefresh = function() {
+        Calendar.fetch().success( function( response ) {
+            var modifiedEvents = [];
+            
+            for ( var event in response.events ) {
+                var start = new Date( event.start );
+                var end = new Date( event.end );
 
-});
+                console.log( start );
+                console.log( end );
+
+                var modifiedEvent = event;
+                modifiedEvent.start = start;
+                modifiedEvent.end = end;
+
+                modifiedEvents.push( modifiedEvent );
+            }
+
+            Calendar.set( modifiedEvents );
+        } );
+    };
+
+    $scope.doRefresh();
+
+    var calendar = Calendar.get();
+
+    $scope.eventSources = calendar.events;
+} );
---
+++ www/js/services/calendar.js
@@ -0,0 +1,24 @@
+angular.module('roots.services')
+
+.factory('Calendar', function($http, $rootScope, $location, $localstorage) {
+
+  var calendar = {};
+
+  if(typeof $localstorage.get("calendar") !== 'undefined' && $localstorage.get("calendar")!==null){
+    calendar = $localstorage.getObject("calendar");
+  }
+
+  return {
+    set: function(data){
+      calendar = data;
+      $localstorage.setObject('calendar', data);
+    },
+    get: function(){
+      return calendar;
+    },
+    fetch: function(){
+      return $http.jsonp( encodeURI( api+'user/get_events?callback=JSON_CALLBACK&insecure=cool' ) );
+    }
+  };
+
+});
